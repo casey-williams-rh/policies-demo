f69dd4d6c696286d0019922ed2b7221c089144d6
casey-williams-rh <76960530+casey-williams-rh@users.noreply.github.com>
2021-08-10 16:46:15 -0400

    Add files via upload

---
+++ config/test.webpack.config.ts
@@ -0,0 +1,15 @@
+import config from '@redhat-cloud-services/frontend-components-config';
+import { resolve } from 'path';
+
+import { updateTsLoaderRule } from './common.webpack.config';
+
+const { config: webpackConfig, plugins } = config({
+    rootFolder: resolve(__dirname, '../')
+});
+
+updateTsLoaderRule(webpackConfig.module.rules);
+
+module.exports = {
+    ...webpackConfig,
+    plugins
+};
